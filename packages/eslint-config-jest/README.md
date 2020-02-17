50ceb0e44810ff45c7b58c230ccb55a1985a0435
Guillaume Clochard <guillaume.clochard@iadvize.com>
2020-02-17 19:50:20 +0100

    feat(eslint-config-jest): first commit

---
+++ packages/eslint-config-jest/README.md
@@ -0,0 +1,21 @@
+# @iadvize-oss/eslint-config-jest
+
+## Install
+
+Add this dependency to your repository and the eslint peer-dependency :
+
+```bash
+npm add --save-dev @iadvize-oss/eslint-config-jest eslint
+```
+
+## Usage
+
+Create a `.eslintrc` file in your project root with this content
+
+```json
+{
+  "extends": [
+    "@iadvize-oss/eslint-config-jest"
+  ]
+}
+```
---
+++ packages/eslint-config-jest/index.js
@@ -0,0 +1,43 @@
+const JEST_FILES = [
+  '**/*.test.@(j|t)s?(x)',
+  'tests/**/*.@(j|t)s?(x)',
+  '**/tests/**/*.@(j|t)s?(x)',
+  '**/*.spec.@(j|t)s?(x)',
+];
+
+module.exports = {
+  plugins: ['jest'],
+  overrides: [
+    {
+      files: JEST_FILES,
+      excludedFiles: '*.mocha.*',
+      env: {
+        jest: true,
+        'jest/globals': true,
+      },
+      rules: {
+        'no-unused-expressions': 'off',
+        'import/no-extraneous-dependencies': 'off',
+
+        'jest/no-alias-methods': 'error',
+        'jest/no-disabled-tests': 'warn',
+        'jest/no-focused-tests': 'error',
+        'jest/no-identical-title': 'error',
+        'jest/no-jest-import': 'error',
+        'jest/no-jasmine-globals': 'error',
+        'jest/no-test-prefixes': 'error',
+        'jest/valid-describe': 'off',
+        'jest/valid-expect': 'error',
+        'jest/valid-expect-in-promise': 'error',
+        'jest/require-to-throw-message': 'error',
+        'jest/prefer-to-have-length': 'error',
+        'jest/prefer-to-be-undefined': 'error',
+        'jest/prefer-to-be-null': 'error',
+        'jest/no-truthy-falsy': 'error',
+        'jest/prefer-to-contain': 'error',
+        'jest/prefer-called-with': 'error',
+        'jest/no-test-callback': 'error',
+      },
+    },
+  ],
+};

d0a8b054422d12f2436093303621d245b7775b85
Guillaume Clochard <guillaume.clochard@iadvize.com>
2020-02-17 20:44:45 +0100

    feat(eslint-config-redux-saga): first commit

---
+++ packages/eslint-config-redux-saga/index.js
@@ -0,0 +1,22 @@
+const TESTS_FILES = [
+  '**/*.test.js*',
+  'tests/**/*.js*',
+  '**/tests/**/*.js*',
+  '**/*.spec.js*',
+];
+
+module.exports = {
+  rules: {
+    'redux-saga/no-yield-in-race': 'error',
+    'redux-saga/yield-effects': 'error',
+  },
+  overrides: [
+    {
+      files: [...TESTS_FILES],
+      rules: {
+        'redux-saga/no-unhandled-errors': 'off',
+      },
+    },
+  ],
+  plugins: ['redux-saga'],
+};
